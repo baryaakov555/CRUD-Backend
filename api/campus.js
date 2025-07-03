602a38d17be78ab768d024fa72b3cf69441b2d31
Bar Yaakov <baryaakov5050@gmail.com>
2025-07-02 23:14:31 -0400

    added a way for the user to request createion of campuses

--- api/campus.js
+++ api/campus.js
@@ -2,14 +2,25 @@ const express = require("express");
 const router = express.Router();
 const { Campus, Student } = require("../database");
 
-router.get("/", async (req, res, next) => {
-    try {
-        const campuses = await Campus.findAll({ include: Student });
-        res.json(campuses);
-    }
-    catch(err) {
-        next(err);
-    }
+router.get("/", async (req, res) => {
+  try {
+    const campuses = await Campus.findAll({ include: Student });
+    res.json(campuses);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: error.message });
+  }
+});
+
+router.post("/", async (req, res) => {
+  try {
+    const camputInput = req.body;
+    const newCampuse = await Campus.create(camputInput);
+    res.json(newCampuse);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: error.message });
+  }
 });
 
 module.exports = router;
--- api/student.js
+++ api/student.js
@@ -3,13 +3,12 @@ const router = express.Router();
 const { Student } = require("../database");
 
 router.get("/", async (req, res, next) => {
-    try {
-        const students = await Student.findAll();
-        res.json(students);
-    }
-    catch(err) {
-        next(err);
-    }
+  try {
+    const students = await Student.findAll();
+    res.json(students);
+  } catch (err) {
+    next(err);
+  }
 });
 
 module.exports = router;
